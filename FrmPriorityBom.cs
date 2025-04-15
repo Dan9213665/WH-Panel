f4a85376890512a900e2e93544e1ff438a500e6e
lgt <lgt@RT12>
2025-04-15 11:08:31 +0300

    UPDATED 202504151108

--- FrmPriorityBom.cs
+++ FrmPriorityBom.cs
@@ -283,7 +283,12 @@ namespace WH_Panel
             }
             else if (serial.SERIALSTATUSDES == "הרכבה בחוסר")
             {
-                e.Graphics.FillRectangle(Brushes.DarkOrange, e.Bounds);
+                e.Graphics.FillRectangle(Brushes.DarkViolet, e.Bounds);
+                e.Graphics.DrawString(serial.ToString(), e.Font, Brushes.Black, e.Bounds);
+            }
+            else if (serial.SERIALSTATUSDES == "טרם נשלח קיט")
+            {
+                e.Graphics.FillRectangle(Brushes.DarkSlateGray, e.Bounds);
                 e.Graphics.DrawString(serial.ToString(), e.Font, Brushes.Black, e.Bounds);
             }
             else if (serial.SERIALSTATUSDES == "הוקפאה")
