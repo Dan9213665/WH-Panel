2313c0cda20f497499a61622c4e1f06bbb34b820
lgt <lgt@RT12>
2025-11-30 09:22:21 +0200

    UPDATED 202511300922

--- FrmPriorityAPI.cs
+++ FrmPriorityAPI.cs
@@ -486,7 +486,7 @@ namespace WH_Panel
             //MessageBox.Show($"User detected : {curentUser}\n");
             if (curentUser == "lgt01")
             {
-                thecredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{settings.Api3Password}:{settings.Api3Password}"));
+                thecredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{settings.Api3Username}:{settings.Api3Password}"));
             }
             else if (curentUser == "rbtwh")
             {
