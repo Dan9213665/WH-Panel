ac16fb3bc961f964b94d9fb1643b7df76daffd85
lgt <lgt@RT12>
2026-07-28 17:14:38 +0300

    UPDATED 202607281714

--- FrmPriorityAPI.cs
+++ FrmPriorityAPI.cs
@@ -569,7 +569,7 @@ namespace WH_Panel
         // 1. Blacklist uses friendly names cleanly
         public static HashSet<string> SessionBlacklist = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
 {
-   "Yuri_G","master"
+   "Yuri_G","master","Jessica"
 };
 
         public string SelectTheCredentialsByLoggedUser(AppSettings settings)
